a95a5b4ad2a562d90b5aa454cef0514e72d7de50
yuriy-npm <gnatko.yr@gmail.com>
2023-09-06 14:15:34 +0300

    edited connection DB

--- db.js
+++ db.js
@@ -4,6 +4,11 @@ module.exports = new Sequelize (
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
+    // {
+    //     dialect: 'postgres',
+    //     host: ec2-54-84-182-168.compute-1.amazonaws.com,
+    //     port: 5432,
+    // }
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
