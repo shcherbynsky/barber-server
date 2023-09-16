11d06d4ceda1b6b8562871e05a13d2597b81f29d
yuriy-npm <gnatko.yr@gmail.com>
2023-09-16 17:00:34 +0300

    cors url edited

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
