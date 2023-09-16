2ac3265758478702943a3312d2e0f98b913ec0a2
yuriy-npm <gnatko.yr@gmail.com>
2023-09-16 16:30:26 +0300

    edit cors

--- index.js
+++ index.js
@@ -8,21 +8,23 @@ const cors = require('cors')
 const path = require('path')
 const router = require('./routes/index')
 const filePathMiddleware = require('./services/cloudinary')
-// const errorHandler = require('./middleWare/errorHandlingMiddleWare')
 
-// const PORT = 5432
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
-// app.use(filePathMiddleware(path.resolve(__dirname, 'static')))
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router) 
-// app.use(errorHandler)
 
 
 const start = async () => {
